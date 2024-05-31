b41f1c498a26bd0a78bc6ef36061d13fd9a39052
Veronika Nychyporuk <veronikanychyporuk@gmail.com>
2024-05-31 09:41:27 +0300

    add iCategoryModelTest

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Category, Image
+from django.core.files.uploadedfile import SimpleUploadedFile
+from datetime import date
 
-# Create your tests here.
+class CategoryModelTest(TestCase):
+    def test_category_creation(self):
+        category = Category.objects.create(name="Test Category")
+        self.assertEqual(category.name, "Test Category")
+        self.assertEqual(str(category), "Test Category")
+
