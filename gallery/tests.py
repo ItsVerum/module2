7c1f1869968f0515107e82900adb2a781002fc1d
Veronika Nychyporuk <veronikanychyporuk@gmail.com>
2024-05-31 09:48:16 +0300

    add ImageModelTest

--- gallery/tests.py
+++ gallery/tests.py
@@ -9,4 +9,25 @@ class CategoryModelTest(TestCase):
         self.assertEqual(category.name, "Test Category")
         self.assertEqual(str(category), "Test Category")
 
+class ImageModelTest(TestCase):
 
+    def setUp(self):
+        self.category = Category.objects.create(name="Test Category")
+
+    def test_image_creation(self):
+        image_content = SimpleUploadedFile(name='test_image.jpg', content=b'', content_type='image/jpeg')
+
+        image = Image.objects.create(
+            title="Test Image",
+            image=image_content,
+            created_date=date.today(),
+            age_limit=18
+        )
+        image.categories.add(self.category)
+
+        self.assertEqual(image.title, "Test Image")
+        self.assertTrue(image.image.name.startswith('images/test_image'))
+        self.assertEqual(image.created_date, date.today())
+        self.assertEqual(image.age_limit, 18)
+        self.assertIn(self.category, image.categories.all())
+        self.assertEqual(str(image), "Test Image")
